82105143acc06f8456a5a8038ab15a5eb0e49a0a
Zsolt Berenyi <zsolt888b@gmail.com>
2022-05-25 18:40:16 +0200

    google analytics

--- app/src/main/java/com/example/brewery/UI/main/MainActivity.kt
+++ app/src/main/java/com/example/brewery/UI/main/MainActivity.kt
@@ -9,17 +9,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Observer
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity(){
+    private lateinit var firebaseAnalytics: FirebaseAnalytics;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics;
 
         setContent {
             CompositionLocalProvider() {
-                Main(viewModel = hiltViewModel())
+                Main(viewModel = hiltViewModel());
             }
         }
     }
